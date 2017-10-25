e054ccd206fa9ccdba56fdd5e83ae8091590d04a
dk\ivd <ivd@sitecore.net>
2017-10-25 12:16:47 +0800

    #31187: Solve the issue

---
+++ src/Sitecore.Support.31187/SearchRepository.cs
@@ -0,0 +1,320 @@
+﻿
+namespace Sitecore.Support.Commerce.UX.CustomerOrderManager.Repositories
+{
+    using Sitecore.ContentSearch;
+    using Sitecore.ContentSearch.Linq;
+    using Sitecore.Diagnostics;
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using Sitecore.Commerce.UX.CustomerOrderManager;
+    using Sitecore.Commerce.UX.CustomerOrderManager.Repositories;
+
+    /// <summary>
+    /// A class that represents a repository for search.
+    /// </summary>
+    public class SearchRepository : ISearchRepository
+    {
+        private const string OrdersIndexName = "commerce_orders_index";
+        private const string CustomersIndexName = "commerce_userprofiles_index_master";
+
+        private const string OrderPlacedDate = "orderplaceddate";
+
+        /// <summary>
+        /// Gets the search results for the given entity type and search term. The optional parentId
+        /// parameter restricts the search to the child elements specific entity instance.
+        /// </summary>
+        /// <param name="itemType">The entity type</param>
+        /// <param name="searchTerm">The search term</param>
+        /// <param name="parentId">The parent id</param>
+        /// <param name="sortDirection">The sort direction</param>
+        /// <param name="sortProperty">The sort property</param>
+        /// <param name="pageIndex">The page index</param>
+        /// <param name="pageSize">The page size</param>
+        /// <param name="environment">The environment.</param>
+        /// <param name="totalItemCount">The total item count</param>
+        /// <param name="requestedProperties">The requested properties</param>
+        /// <returns>
+        /// The search results
+        /// </returns>
+        /// <exception cref="System.InvalidOperationException">itemType not recognized</exception>
+        public List<object> GetSearchResults(string itemType, string searchTerm, string parentId, string sortDirection, string sortProperty, int pageIndex, int pageSize, string environment, out int totalItemCount, List<string> requestedProperties)
+        {
+            Assert.IsNotNullOrEmpty(itemType, "The parameter itemType cannot be empty.");
+
+            if (searchTerm != null)
+            {
+                searchTerm = searchTerm.Trim();
+            }
+
+            totalItemCount = 0;
+
+            if (itemType.Equals("customer", StringComparison.OrdinalIgnoreCase))
+            {
+                return this.GetCustomerSearchResults(
+                    searchTerm,
+                    sortDirection,
+                    sortProperty,
+                    pageIndex,
+                    pageSize,
+                    environment,
+                    out totalItemCount,
+                    requestedProperties);
+            }
+
+            if (itemType.Equals("order", StringComparison.OrdinalIgnoreCase))
+            {
+                return this.GetOrderSearchResults(
+                    searchTerm,
+                    sortDirection,
+                    sortProperty,
+                    pageIndex,
+                    pageSize,
+                    environment,
+                    out totalItemCount,
+                    requestedProperties);
+            }
+
+            throw new InvalidOperationException("itemType not recognized");
+        }
+
+        /// <summary>
+        /// Gets the order search results.
+        /// </summary>
+        /// <param name="searchTerm">The search term.</param>
+        /// <param name="sortDirection">The sort direction.</param>
+        /// <param name="sortProperty">The sort property.</param>
+        /// <param name="pageIndex">Index of the page.</param>
+        /// <param name="pageSize">Size of the page.</param>
+        /// <param name="artifactStoreId">The environment.</param>
+        /// <param name="totalItemCount">The total item count.</param>
+        /// <param name="requestedProperties">The requested properties.</param>
+        /// <returns>A list of orders</returns>
+        private List<object> GetOrderSearchResults(string searchTerm, string sortDirection, string sortProperty, int pageIndex, int pageSize, string artifactStoreId, out int totalItemCount, List<string> requestedProperties)
+        {
+            ISearchIndex ordersIndex = ContentSearchManager.GetIndex(OrdersIndexName);
+
+            using (var context = ordersIndex.CreateSearchContext())
+            {
+                var searchQuery = context.GetQueryable<OrderSearchResultItem>();
+
+                if (!string.IsNullOrWhiteSpace(searchTerm))
+                {
+                    searchQuery = searchQuery.Where(
+                        r => (r.OrderConfirmationId.Equals(searchTerm, StringComparison.OrdinalIgnoreCase) || r.Email.Equals(searchTerm, StringComparison.OrdinalIgnoreCase))
+                        && r.ArtifactStoreId.Equals(artifactStoreId, StringComparison.OrdinalIgnoreCase));
+                }
+                else
+                {
+                    searchQuery = searchQuery.Where(r => r.ArtifactStoreId.Equals(artifactStoreId, StringComparison.OrdinalIgnoreCase));
+                }
+
+                totalItemCount = searchQuery.Count();
+
+                var isPagingSpecified = pageSize > 0 && pageIndex >= 0;
+
+                searchQuery = this.ApplySorting(searchQuery, sortProperty, sortDirection);
+
+                if (isPagingSpecified)
+                {
+                    searchQuery = searchQuery.Skip(pageIndex * pageSize).Take(pageSize);
+                }
+
+                var searchResults = searchQuery.GetResults();
+
+                var resultsItems = searchResults.Select(r => this.CreateOrderResult(r.Document, requestedProperties));
+
+                return resultsItems.ToList<object>();
+            }
+        }
+
+        private IQueryable<OrderSearchResultItem> ApplySorting(IQueryable<OrderSearchResultItem> searchQuery, string sortProperty, string sortDirection)
+        {
+            if (string.IsNullOrWhiteSpace(sortProperty))
+            {
+                return searchQuery;
+            }
+
+            if (sortProperty == OrderPlacedDate)
+            {
+                return sortDirection == "Asc"
+                    ? searchQuery.OrderBy(order => order.OrderDate)
+                    : searchQuery.OrderByDescending(order => order.OrderDate);
+            }
+
+            return sortDirection == "Asc" ? 
+                searchQuery.OrderBy(order => order[sortProperty]) : 
+                searchQuery.OrderByDescending(order => order[sortProperty]);
+        }
+
+        /// <summary>
+        /// Gets the customer search results.
+        /// </summary>
+        /// <param name="searchTerm">The search term.</param>
+        /// <param name="sortDirection">The sort direction.</param>
+        /// <param name="sortProperty">The sort property.</param>
+        /// <param name="pageIndex">Index of the page.</param>
+        /// <param name="pageSize">Size of the page.</param>
+        /// <param name="environment">The environment.</param>
+        /// <param name="totalItemCount">The total item count.</param>
+        /// <param name="requestedProperties">The requested properties.</param>
+        /// <returns>A list of Customers</returns>
+        private List<object> GetCustomerSearchResults(string searchTerm, string sortDirection, string sortProperty, int pageIndex, int pageSize, string environment, out int totalItemCount, List<string> requestedProperties)
+        {
+            ISearchIndex customersIndex = ContentSearchManager.GetIndex(CustomersIndexName);
+
+            using (var context = customersIndex.CreateSearchContext())
+            {
+                var searchQuery = context.GetQueryable<CustomerSearchResultItem>();
+
+                if (!string.IsNullOrWhiteSpace(searchTerm))
+                {
+                    if (searchTerm.Contains("*"))
+                    {
+                        if (searchTerm.EndsWith("*", StringComparison.OrdinalIgnoreCase))
+                        {
+                            searchQuery = searchQuery.Where(
+                                r => (r.UserId.Equals(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                                r.Email.StartsWith(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                                r.FirstName.StartsWith(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                                r.LastName.StartsWith(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                                r.Content.Contains(searchTerm)));
+                        }
+                        else if (searchTerm.StartsWith("*", StringComparison.OrdinalIgnoreCase))
+                        {
+                            searchQuery = searchQuery.Where(
+                                r => (r.UserId.Equals(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                                r.Email.EndsWith(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                                r.FirstName.EndsWith(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                                r.LastName.EndsWith(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                                r.Content.Contains(searchTerm)));
+                        }
+                    }
+                    else
+                    {
+                        searchQuery = searchQuery.Where(
+                            r => (r.UserId.Equals(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                            r.Email.Equals(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                            r.FirstName.Equals(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                            r.LastName.Equals(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                            r.Content.Equals(searchTerm)));
+                    }
+                }
+
+                totalItemCount = searchQuery.Count();
+
+                var isSortingSpecified = !string.IsNullOrWhiteSpace(sortProperty);
+                var isPagingSpecified = pageSize > 0 && pageIndex >= 0;
+
+                if (isSortingSpecified)
+                {
+                    if (sortDirection == "Asc")
+                    {
+                        searchQuery = searchQuery.OrderBy(customer => customer[sortProperty]);
+                    }
+                    else
+                    {
+                        searchQuery = searchQuery.OrderByDescending(customer => customer[sortProperty]);
+                    }
+                }
+
+                if (isPagingSpecified)
+                {
+                    searchQuery = searchQuery.Skip(pageIndex * pageSize).Take(pageSize);
+                }
+
+                var searchResults = searchQuery.GetResults();
+
+                var resultsItems = searchResults.Select(r => this.CreateCustomerResult(r.Document, requestedProperties));
+
+                return resultsItems.ToList<object>();
+            }
+        }
+
+        /// <summary>
+        /// Creates the customer result.
+        /// </summary>
+        /// <param name="resultItem">The result item.</param>
+        /// <param name="requestedFields">The requested fields.</param>
+        /// <returns>A list of Customer results</returns>
+        private object CreateCustomerResult(CustomerSearchResultItem resultItem, List<string> requestedFields)
+        {
+            // Always include UserID, LastName, FirstName and email
+            var result = new Dictionary<string, object>();
+            result.Add("Id", resultItem.UserId);
+            result.Add("first_name", resultItem.FirstName);
+            result.Add("last_name", resultItem.LastName);
+            result.Add("email_address", resultItem.Email);
+            if (!resultItem.ExternalId.Contains("CommerceUsers"))
+            {
+                result.Add("ItemId", resultItem.ExternalId);
+            }
+            else
+            {
+                result.Add("ItemId", resultItem.UserId);
+            }
+
+            result.Add("Template", "Customer");
+
+            // TODO: Get the last order date from the customer's orders
+            result.Add("LastOrderDate", DateUtil.ToIsoDate(DateTime.Now));
+            result.Add("customertargeturl", "/sitecore/client/Applications/CustomerOrderManager/Customer?target=" + resultItem.UserId);
+
+            requestedFields.ForEach(f =>
+            {
+                if (!result.ContainsKey(f.ToLower(CultureInfo.InvariantCulture)))
+                {
+                    var fieldValue = resultItem.Fields[f];
+
+                    if (fieldValue.GetType() == typeof(DateTime))
+                    {
+                        result.Add(f, DateUtil.ToIsoDate((DateTime)fieldValue));
+                    }
+                    else
+                    {
+                        result.Add(f, resultItem.Fields[f]);
+                    }
+                }
+            });
+
+            return result;
+        }
+
+        /// <summary>
+        /// Creates the order result.
+        /// </summary>
+        /// <param name="resultItem">The result item.</param>
+        /// <param name="requestedFields">The requested fields.</param>
+        /// <returns>A list of Order results</returns>
+        private object CreateOrderResult(OrderSearchResultItem resultItem, List<string> requestedFields)
+        {
+            // Always include order id, order confirmation, order date, and orderTargetUrl
+            var result = new Dictionary<string, object>();
+
+            result.Add("orderid", resultItem.OrderId);
+            result.Add("orderconfirmationid", resultItem.OrderConfirmationId);
+            result.Add("ordertargeturl", "/sitecore/client/Applications/CustomerOrderManager/Order?target=" + resultItem.OrderId);
+            result.Add(OrderPlacedDate, DateUtil.ToIsoDate(resultItem.OrderDate));
+
+            requestedFields.ForEach(f =>
+            {
+                if (!result.ContainsKey(f.ToLower(CultureInfo.InvariantCulture)))
+                {
+                    var fieldValue = resultItem.Fields[f];
+
+                    if (fieldValue.GetType() == typeof(DateTime))
+                    {
+                        result.Add(f, DateUtil.ToIsoDate((DateTime)fieldValue));
+                    }
+                    else
+                    {
+                        result.Add(f, resultItem.Fields[f]);
+                    }
+                }
+            });
+
+            return result;
+        }
+    }
+}
---
+++ src/Sitecore.Support.31187/SupportCustomerOrderSearchController.cs
@@ -0,0 +1,237 @@
+﻿
+namespace Sitecore.Support.Commerce.UX.CustomerOrderManager.Controllers
+{
+    using Sitecore.Commerce.UX.CustomerOrderManager.Repositories;
+    using Sitecore.Diagnostics;
+    using Sitecore.Web;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using Sitecore.Commerce.UX.Shared.Controllers;
+
+    /// <summary>
+    /// A controller class for search operations.
+    /// </summary>
+    public class SupportCustomerOrderSearchController : BaseController
+    {
+        /// <summary>
+        /// The repository for this controller.
+        /// </summary>
+        private ISearchRepository repository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomerOrderSearchController"/> class.
+        /// </summary>
+        /// <param name="repository">The repository</param>
+        public SupportCustomerOrderSearchController(ISearchRepository repository)
+        {
+            Assert.IsNotNull(repository, "repository");
+            this.repository = repository;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomerOrderSearchController"/> class.
+        /// </summary>
+        public SupportCustomerOrderSearchController()
+            : this(new Sitecore.Support.Commerce.UX.CustomerOrderManager.Repositories.SearchRepository())
+        {
+            // TODO: IOC/DI
+        }
+
+        /// <summary>
+        /// Gets the repository for this controller.
+        /// </summary>
+        protected ISearchRepository Repository
+        {
+            get
+            {
+                return this.repository;
+            }
+        }
+
+        /// <summary>
+        /// Gets the search results matching the given search term, entity type, and parent id.
+        /// </summary>
+        /// <param name="itemType">The type of entity.</param>
+        /// <param name="searchTerm">The search term.</param>
+        /// <param name="parentId">The parent id</param>
+        /// <returns>The customer search results matching the search term.</returns>
+        public JsonResult GetSearchResults(string itemType, string searchTerm, string parentId = null)
+        {
+            var sortDirection = this.GetRequestSortDirection();
+            var sortProperty = this.GetRequestSortProperty();
+            var pageIndex = this.GetRequestPageIndex();
+            var pageSize = this.GetRequestPageSize();
+            var requestedProperties = this.GetRequestedFields();
+            var language = GetRequestedLanguage();
+            var currency = GetRequestedCurrency();
+            var environment = GetRequestedEnvironment();
+
+            var totalItemCount = 0;
+            var results = this.Repository.GetSearchResults(
+                itemType,
+                searchTerm,
+                parentId,
+                sortDirection,
+                sortProperty,
+                pageIndex,
+                pageSize,
+                environment,
+                out totalItemCount,
+                requestedProperties);
+
+            var queryResponse = new { Items = results.ToArray(), TotalItemCount = totalItemCount };
+            return Json(queryResponse);
+        }
+
+        /// <summary>
+        /// Gets the requested environment.
+        /// </summary>
+        /// <returns>The Environment name or an empty string</returns>
+        private string GetRequestedEnvironment()
+        {
+            var headerString = WebUtil.GetFormValue("Headers");
+            if (!string.IsNullOrWhiteSpace(headerString))
+            {
+                var headers = headerString.Split('|');
+                foreach (var item in headers)
+                {
+                    var headerValues = item.Split(':');
+                    if (headerValues[0] == "Environment")
+                    {
+                        return headerValues[1];
+                    }
+                }
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Gets the requested currency.
+        /// </summary>
+        /// <returns>The currency in use or an empty string</returns>
+        private string GetRequestedCurrency()
+        {
+            var currency = WebUtil.GetFormValue("Currency");
+            if (!string.IsNullOrWhiteSpace(currency))
+            {
+                return currency;
+            }
+
+            return string.Empty;
+        }
+
+        private string GetRequestedLanguage()
+        {
+            var language = WebUtil.GetFormValue("Language");
+            if (!string.IsNullOrWhiteSpace(language))
+            {
+                return language;
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Gets the requested fields
+        /// </summary>
+        /// <returns>A list of strings that represent the requested fields.</returns>
+        private List<string> GetRequestedFields()
+        {
+            var delimitedFields = WebUtil.GetFormValue("fields");
+            var requestedFields = new List<string>();
+
+            if (!string.IsNullOrWhiteSpace(delimitedFields))
+            {
+                var fields = delimitedFields.Split('|');
+                foreach (var field in fields)
+                {
+                    if (!string.IsNullOrWhiteSpace(field))
+                    {
+                        requestedFields.Add(field);
+                    }
+                }
+            }
+
+            return requestedFields;
+        }
+
+        /// <summary>
+        /// Gets the requested sort direction.
+        /// </summary>
+        /// <returns>The sort direction.</returns>
+        private string GetRequestSortDirection()
+        {
+            var sorting = WebUtil.GetFormValue("Sorting");
+            if (!string.IsNullOrWhiteSpace(sorting))
+            {
+                // By SPEAK convention, the sort direction is
+                // the first character of the Sorting variable
+                var sortDirection = sorting[0];
+                if (sortDirection == 'a')
+                {
+                    return "Asc";
+                }
+
+                if (sortDirection == 'd')
+                {
+                    return "Desc";
+                }
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Gets the requested sort property.
+        /// </summary>
+        /// <returns>The sort property.</returns>
+        private string GetRequestSortProperty()
+        {
+            var sorting = WebUtil.GetFormValue("Sorting");
+            if (!string.IsNullOrWhiteSpace(sorting) && sorting.Length > 1)
+            {
+                // By SPEAK convention, the sort property is
+                // prefixed with the sort direction, so ignore the first character
+                var sortProperty = sorting.Substring(1);
+                return sortProperty;
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Gets the requested page index.
+        /// </summary>
+        /// <returns>The page index.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1806:DoNotIgnoreMethodResults", MessageId = "System.Int32.TryParse(System.String,System.Int32@)", Justification = "Out parameter is set to default value in case conversion fails.")]
+        private int GetRequestPageIndex()
+        {
+            int pageIndex = 0;
+            var pageIndexString = WebUtil.GetFormValue("PageIndex");
+            if (!string.IsNullOrWhiteSpace(pageIndexString))
+            {
+                var parseSucceeded = int.TryParse(pageIndexString, out pageIndex);
+            }
+
+            return pageIndex;
+        }
+
+        /// <summary>
+        /// Gets the requested page size.
+        /// </summary>
+        /// <returns>The page size.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1806:DoNotIgnoreMethodResults", MessageId = "System.Int32.TryParse(System.String,System.Int32@)", Justification = "Out parameter is set to default value in case conversion fails.")]
+        private int GetRequestPageSize()
+        {
+            int pageSize = 0;
+            var pageSizeString = WebUtil.GetFormValue("PageSize");
+            if (!string.IsNullOrWhiteSpace(pageSizeString))
+            {
+                int.TryParse(pageSizeString, out pageSize);
+            }
+
+            return pageSize;
+        }
+    }
+}
